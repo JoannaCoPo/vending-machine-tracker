467221055de25e78a1fcbf7cd26ea4958b22c4b7
JoannaCoPo <jococopo@gmail.com>
2021-05-31 23:35:25 -0600

    Complete user story 2

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -3,4 +3,8 @@ class Machine < ApplicationRecord
   belongs_to :owner
   has_many :snack_machines
   has_many :snacks, through: :snack_machines
+
+  def average_snack_price
+    self.snacks.average(:price)
+  end
 end
--- spec/features/machines/show_spec.rb
+++ spec/features/machines/show_spec.rb
@@ -4,15 +4,32 @@ RSpec.describe "the Machine show page" do
   it "should display all the machines snacks with their prices" do
     owner = Owner.create(name: "Sam's Snacks")
     dons  = owner.machines.create(location: "Don's Mixed Drinks")
-    snack1 = Snack.create!(name: "Sugar", price: 1)
-    snack2 = Snack.create!(name: "Sugar Drink", price: 2)
+    snack1 = Snack.create!(name: "Pina Coladinator", price: 4)
+    snack2 = Snack.create!(name: "Cola Bevvie", price: 2)
     dons.snacks.push(snack1, snack2)
 
     visit "/machines/#{dons.id}"
-save_and_open_page
-    expect(page).to have_content(snack1.name)
-    expect(page).to have_content(snack1.price)
-    expect(page).to have_content(snack2.name)
-    expect(page).to have_content(snack2.price)
+
+    within("#snack-#{snack1.id}") do
+      expect(page).to have_content(snack1.name)
+      expect(page).to have_content(snack1.price)
+    end
+    within("#snack-#{snack2.id}") do
+      expect(page).to have_content(snack2.name)
+      expect(page).to have_content(snack2.price)
+    end
+  end
+
+  it "displays the average price for all snacks in the machine" do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    snack1 = Snack.create!(name: "Pina Coladinator", price: 4)
+    snack2 = Snack.create!(name: "Cola Bevvie", price: 2)
+    snack3 = Snack.create!(name: "Can of Mimosa", price: 3.50)
+    dons.snacks.push(snack1, snack2, snack3)
+
+    visit "/machines/#{dons.id}"
+
+    expect(page).to have_content("Average Price: $3.17")
   end
 end
